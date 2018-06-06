58e2658e86fd62d782251a66189e11a4ad3969c9
Samuellks <Samuellks@users.noreply.github.com>
2018-06-07 02:30:15 +0800

    ss

--- src/Ass2.java
+++ src/Ass2.java
@@ -3,12 +3,10 @@ public class Ass2 {
 
 	public static void main(String [] args) {
 	System.out.printf("Suk Mah dik");
-<<<<<<< HEAD
 	//test git
 	//danson test
 	//stoned or not
 	//u stoned?
 	//test merge
-
 	}
 }
