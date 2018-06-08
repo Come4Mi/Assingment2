d0072c8433455b75b81b501f5e278a242cac5ccb
Yyueei <w.leaf@hotmail.com>
2018-06-08 14:09:35 +0800

    simple

--- src/Ass2.java
+++ src/Ass2.java
@@ -4,19 +4,6 @@ public class Ass2 {
 	public static void main(String [] args) {
 	System.out.printf("Suk Mah dik");
 	//test git
-<<<<<<< HEAD
-	//Ass2 package
-	//josh and vikysjkshdjsjkdask
-=======
-	//danson test
-	//stoned or not
-	//u stoned?
-<<<<<<< HEAD
-	//test merge
-	
-	//hfghbfgfgb
->>>>>>> origin/testBranch
-=======
->>>>>>> 7e558b86495ae1ef722f3698e6f51d2d98c53159
+	//sadasdsadsadas
 	}
 }
